69dd1d169e14170e6678dc1588c7a1a6b0083635
mathemagie <aurelien.fache@gmail.com>
2013-05-02 20:35:12 +0200

    first release

---
+++ www/db.php
@@ -0,0 +1,28 @@
+<?php
+$production = 1;
+
+if ($_SERVER["USER"] == 'aurel') $production = 0;
+if (strstr($_SERVER['HTTP_HOST'],'localhost')) $production = 0;
+
+if (!$production){
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = 'root';
+    ini_set('display_errors', '1');
+    error_reporting(E_ALL ^ E_NOTICE);
+}
+else {
+    ini_set('display_errors', '0');
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = 'aureliendela';
+}
+
+try
+{
+    $db = new PDO('mysql:host=localhost;dbname=api_cantine', $username,$password);
+}
+catch(PDOException $ex) {
+   echo $ex;
+}
+?>
--- www/index.php
+++ www/index.php
@@ -1,3 +1,22 @@
 <?php
-	echo "API cantine";
+	require_once 'db.php';
+	$do = $_GET['do'];
+	$all_method = array('up','down');
+
+	function up() {
+		global $db;
+		$sql = "UPDATE number_person SET number = number + 1";
+  		$sth = $db->query($sql);
+	}
+
+	function down() {
+		global $db;
+		$sql = "UPDATE number_person SET number = number - 1";
+		$sth = $db->query($sql);
+	}
+
+	if ($do == 'up') up();
+	if ($do == 'down') down();
+	echo "OK";
+
 ?>
