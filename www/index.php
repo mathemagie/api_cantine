310e3d1325aee8e2379133a6303509d16be2bc85
mathemagie <aurelien.fache@gmail.com>
2013-05-02 20:52:18 +0200

    add realtime

--- www/index.php
+++ www/index.php
@@ -1,7 +1,15 @@
 <?php
+	require 'pusher-php-server-master/lib/Pusher.php';
 	require_once 'db.php';
+	
 	$do = $_GET['do'];
 	$all_method = array('up','down');
+
+	$app_id = '43155';
+	$app_key = '1fbe9243adcdd1024b64';
+	$app_secret = 'cd1b7210ddb6e46895a2';
+	$pusher = new Pusher( $app_key, $app_secret, $app_id );
+
 
 	function up() {
 		global $db;
@@ -15,8 +23,25 @@
 		$sth = $db->query($sql);
 	}
 
+	function get_total() {
+		global $db;
+
+		$sql = "SELECT number from number_person";
+		$sth = $db->query($sql);
+		$data = array();
+	    foreach($sth as $row) {
+	      $total_number = $row['number'];
+	    }
+	    return $total_number;
+	}
+
 	if ($do == 'up') up();
 	if ($do == 'down') down();
+	
+	$total = get_total();
+	echo $total;
+	echo "<br/>";
+	$pusher->trigger( 'test_channel', 'my_event', $total );
 	echo "OK";
 
 ?>
